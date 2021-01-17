b63215ec7308ab228dd3ca419a929985e5d7a2f4
Tamer Samara <77573013+tamer2312@users.noreply.github.com>
2021-01-17 17:21:04 +0200

    Add files via upload

---
+++ Enter With Invalid Email.py
@@ -0,0 +1,91 @@
+'''
+Authour by : Tamer Samara
+Emails : samaratamer2312@gmail.com
+Phone : 050-8615913
+Date : 05/12/2020
+Enter The FaceBook With Valid Details And Writing To Log And A Result FIle Log
+'''
+
+# import relevant modules
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+import datetime, time
+
+# Define new Test Class
+class InValidEmail:
+    def __init__(self): #Function that print when the test start
+        print(" The Second Test was started...")
+
+
+    def WriteToLog(self, fileName, textToWrite):
+        f = open(fileName, 'a')  # opening file for adding text
+        timeStamp = str(datetime.datetime.now())  # builtin function from datetime module to get current time
+        f.write(timeStamp + " " + textToWrite + '\n')  # writing timestamp + text + \n in order to start a new line!
+        f.close()  # Closing file
+
+    def PrintInvalidEmail(self):
+        self.fileName = "SecondTest.txt" #Write the test result to file
+        self.resultsFile = "TestsResults.txt" # contain all the results for all tests
+        self.WriteToLog(self.fileName,"Starting with FB Test : - Try To Login With Invalid User Name Details.")
+        self.WriteToLog(self.resultsFile, "Starting with Second FB Test : - Try To Login With Invalid User Name Details.")
+
+        try:
+            option = Options()  # Use this 3 lines in order to add option to send or disable facebook notifications.
+            option.add_experimental_option("prefs",{"profile.default_content_setting_values.notifications": 1})  # 1 yes 2 No
+            self.driver = webdriver.Chrome(options=option,executable_path='C:\\driversp\\chromedriver.exe')# Start chrome with the option. chrome will not pop up message
+            self.driver.get("http://www.facebook.com") # log in the website facebook by chrome browser
+            self.driver.maximize_window() # maximize the window to the fit
+            self.WriteToLog(self.fileName, "The Chrome browser Got maximized") #Write the test result to file - secondtest.txt
+            time.sleep(3) #wait 3 second before start the next try
+        except:
+            self.WriteToLog(self.fileName,"ERROR - error occurred during facebook home page upload")#Write the test result to file - secondtest.txt
+            self.WriteToLog(self.resultsFile, "ERROR - error occurred during facebook home page upload")#Write the test result to file - Testresults.txt
+
+        try:
+            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.NAME, "email"))).clear() #wait until 10 seconds till the email field appears and clear the field
+            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.NAME, "email"))).send_keys('ahmadfislas@gmaosk/xom.com')# fill the email field with the details
+            self.WriteToLog(self.fileName,"The Email was entered successfully") #opening file for adding text for first test log - secondtest.txt
+            self.WriteToLog(self.resultsFile, "The Email was entered successfully") #opening file for adding text for first test logtestresult.txt
+            time.sleep(3) #wait 3 second before start the next try
+        except:
+            self.WriteToLog(self.fileName, "Can't find the Email Field--Test failed")#opening file for adding text for first test log - secondtest.txt
+            self.WriteToLog(self.resultsFile, "Can't find the Email Field--Test failed")#opening file for adding text for first test logtestresult.txt
+
+        try:
+            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.NAME, "pass"))).send_keys('312311004tetoo')# wait until 10 seconds till the email field appears and type the password
+            self.WriteToLog(self.fileName,"The Password was entered successfully")#opening file for adding text for first test log - secondtest.txt
+            self.WriteToLog(self.resultsFile,"The Password was entered successfully")#opening file for adding text for first test logtestresult.txt
+            time.sleep(3) #wait 3 second before start the next try
+
+        except:
+            self.WriteToLog(self.fileName,"Can't find the Password Field--Test failed")#opening file for adding text for first test log - secondtest.txt
+            self.WriteToLog(self.resultsFile, "Can't find the Password Field--Test failed")#opening file for adding text for first test logtestresult.txt
+
+        try:
+            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.NAME, "login"))).click() # click on the login button
+            self.WriteToLog(self.fileName, "The login button was clicked!") #opening file for adding text for first test log - secondtest.txt
+            self.WriteToLog(self.resultsFile, "The login button was clicked!") #opening file for adding text for first test logtestresult.txt
+            self.WriteToLog(self.fileName, "The Web Page is Reloading...") #opening file for adding text for first test log - secondtest.txt
+            self.WriteToLog(self.resultsFile, "The Web Page is Reloading...") #opening file for adding text for first test logtestresult.txt
+            time.sleep(15) #wait 3 second before start the next try
+        except:
+            self.WriteToLog(self.fileName,"The login button was not FOUND!")#opening file for adding text for first test log - secondtest.txt
+            self.WriteToLog(self.resultsFile,"The login button was not FOUND!")#opening file for adding text for first test logtestresult.txt
+
+        if 'The email or phone number you’ve entered doesn’t match any account. ' in str(self.driver.page_source): # Searching in the page source if he found the sentences so...
+            self.WriteToLog(self.fileName, "The facebook was not logged in with InValid details- TEST PASS.")#opening file for adding text for first test log - secondtest.txt
+            self.WriteToLog(self.resultsFile, "The facebook was not logged in with InValid details- TEST PASS.")#opening file for adding text for first test logtestresult.txt
+        else:
+            self.WriteToLog(self.fileName, "The facebook was successfully logged in with valid details- TEST FAIL.")#opening file for adding text for first test log - secondtest.txt
+            self.WriteToLog(self.resultsFile, "The facebook was successfully logged in with valid details- TEST FAIL.")#opening file for adding text for first test logtestresult.txt
+        self.driver.get_screenshot_as_file("Invalid email.png") # taking a screen shot
+        self.driver.quit() #quit the chrome
+
+
+# Needed if you would like to run this plan within this file
+#because all the plan is coded under class type
+if __name__=='__main__':
+    InValidEmail().PrintInvalidEmail()
